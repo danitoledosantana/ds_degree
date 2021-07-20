b8b957eaacf358bd6158e276927914bca79faa0d
danitoledosantana <danitoledosantana@gmail.com>
2021-07-19 21:58:49 -0300

    generalizar quantidade de carteiras

--- Python/Projeto01/mod_knn.py
+++ Python/Projeto01/mod_knn.py
@@ -5,22 +5,29 @@ def classifica_perfil_knn(k ,item_no_class, data):
                 data (base classificada usada como referência)                 
         Retorna: lista classificada
     '''    
+
     lista = []
     x=0
     for item_data in data:
         
         saida = []
-        #para cada cpf/carteira não classificado é necessário descobrir os 3 mais proximos e classificar o perfil
-        x = (item_data[2][0]-item_no_class[0]) **2 +  (item_data[2][1]-item_no_class[1]) **2 +  (item_data[2][2]-item_no_class[2]) **2 + (item_data[2][3]-item_no_class[3]) **2
-        x = pow(x,1/2)
-        saida.append(round(x, 2))
+        saida.append(calcularDistancia(item_data[2],item_no_class))
         saida.append(item_data[1])
-        saida.append(item_data[2])        
-
+        saida.append(item_data[2]) 
         lista.append(saida)
                   
     lista.sort()
     return determinaPerfil(lista[0:k])  #determinar a frequencia das classes
+
+def calcularDistancia(cartAtual, cartVizinho):
+    x = []
+    for i in range(len(cartAtual)):
+        x.append((cartAtual[i] - cartVizinho[i])**2)
+    distancia = sum(x)
+    distancia = distancia**.5
+    
+    return distancia
+        
 
 def determinaPerfil(lista):
     moderado = 0
